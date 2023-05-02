857b34a7bd9e679868720cbb4177307b026f8840
ROllerozxa <rollerozxa@voxelmanip.se>
2023-05-02 22:34:47 +0200

    Add falling boxes in the main menu, cycle splashes

--- mainmenu.lua
+++ mainmenu.lua
@@ -17,25 +17,81 @@ local gui = {
 	}
 }
 
+local boxes = {}
+
 local current_splash = 1
 
-function scenes.mainmenu.init()
-	current_splash = math.random(1, #splashes)
+-- Adds a new hittable box into the world, with proper draw function and
+-- physics properties.
+local function newBox(x,y,w,h)
+
+	-- New dynamic rectangle collider, the box!
+	local box = world:newCollider("Rectangle", { x-(w/2),y-(h/2),w,h })
+
+	-- Give the box a random colour, save it to the box object's userdata so
+	-- it can be accessed in the draw method.
+	box.colour = coolRandomColour()
+
+	-- Redefine the box object's draw method, draw a filled box with the colour
+	-- stored in userdata (the colour method variable)
+	function box:draw()
+		love.graphics.setColor(self.colour.r, self.colour.g, self.colour.b)
+		rotatedRectangle('fill', self:getX(), self:getY(), w, h, self:getAngle())
+		love.graphics.setColor(0,0,0)
+		rotatedRectangle('line', self:getX(), self:getY(), w, h, self:getAngle())
+	end
+
+	-- Add the box object to the boxes table (actually a reference) so it can be iterated over.
+	table.insert(boxes, box)
 end
 
-function scenes.mainmenu.update()
+local step = 0
+
+function scenes.mainmenu.init()
+	step = 0
+
+	world = bf.newWorld(0, 90.82*2, true)
+end
+
+function scenes.mainmenu.update(dt)
 	gtk.update(gui)
+
+	world:update(dt)
+
+	if step % 4 == 0 then
+		local lolo = math.random(0, base_resolution.x)
+		--local lolo = (step*10) % base_resolution.x
+		newBox(lolo, -20, 40, 40)
+	end
+
+	if step % 340 == 0 then
+		current_splash = math.random(1, #splashes)
+	end
+
+	for key, box in pairs(boxes) do
+		local x, y = box:getPosition()
+		if y > base_resolution.y + 64 then
+			box:destroy()
+			boxes[key] = nil
+		end
+	end
+
+	step = step + 1
 end
 
 function scenes.mainmenu.draw()
 	drawBG(44/255, 100/255, 141/255)
+
+	world:draw()
+
+	love.graphics.setLineWidth(4)
 
 	gtk.draw(gui)
 
 	love.graphics.setFont(fonts.sans.biggest)
 	printOutlined("Box Smasher", 350, 53, 6)
 
-	love.graphics.setFont(fonts.sans.medium)
-	love.graphics.setColor(1,1,0)
+	love.graphics.setFont(fonts.sans.big)
+	love.graphics.setColor(1,1,0.3)
 	drawCenteredTextRot(320, 135, 400, 200, splashes[current_splash], -math.pi/8, step)
 end
