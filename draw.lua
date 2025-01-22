1e450444ecc472d11a312ecd1577e14caea3238d
ROllerozxa <rollerozxa@voxelmanip.se>
2025-01-23 00:02:21 +0100

    Move background drawing functions to draw.lua

--- draw.lua
+++ draw.lua
@@ -1,6 +1,7 @@
 
 draw = {}
 
+-- Draw a googly-eyed ball
 function draw.ball(x, y, angle, colour, radius)
 	radius = radius or 30
 
@@ -35,6 +36,7 @@ function draw.ball(x, y, angle, colour, radius)
 		2)
 end
 
+-- Draw a box
 function draw.box(x, y, w, h, angle, colour)
 
 	-- Box fill
@@ -50,4 +52,42 @@ function draw.box(x, y, w, h, angle, colour)
 		love.graphics.setFont(fonts.sans.tiny)
 		love.graphics.print('{'..math.floor(x)..','..math.floor(y)..'}', self:getX(), self:getY())
 	end
+end
+
+-- Draw the background rectangle
+function draw.background(r, g, b)
+	-- Copy the current coordinate transform and disable scale
+	-- (Needed to draw the BG properly without influence from window scaling)
+	love.graphics.push()
+	love.graphics.origin()
+
+	-- Draw background, keeping in mind the potential offset currently happening.
+	love.graphics.setColor(r, g, b)
+	love.graphics.rectangle('fill', offset.x, offset.y, resolution.x, resolution.y)
+	love.graphics.setColor(1,1,1)
+
+	-- Restore coordinate transform
+	love.graphics.pop()
+end
+
+-- Draw the background letterbox
+function draw.backgroundLetterbox(r, g, b)
+	-- Copy the current coordinate transform and disable scale
+	-- (Needed to draw the BG properly without influence from window scaling)
+	love.graphics.push()
+	love.graphics.origin()
+
+	-- Draw the letterboxed background of the side, if the aspect ratio isn't 16:9.
+	-- The letterboxed "void" is slightly darker and is drawn on top in order to cover
+	-- objects falling into the void, hence why this was split up from drawBG().
+	love.graphics.setColor(r/1.25,g/1.25,b/1.25)
+	love.graphics.rectangle('fill', 0, 0, offset.x, love.graphics.getHeight())
+	love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), offset.y)
+	love.graphics.rectangle('fill', love.graphics.getWidth()-offset.x, 0, offset.x, love.graphics.getHeight())
+	love.graphics.rectangle('fill', 0, love.graphics.getHeight()-offset.y, love.graphics.getWidth(), offset.y)
+
+	love.graphics.setColor(1,1,1)
+
+	-- Restore coordinate transform
+	love.graphics.pop()
 end
