2923ddd677fe24ff7e558baa0c05a42e55f75a16
ROllerozxa <rollerozxa@voxelmanip.se>
2025-01-24 22:14:52 +0100

    Split intro card up into two images

--- scenes/intro.lua
+++ scenes/intro.lua
@@ -12,11 +12,12 @@ end
 function scenes.intro.update(dt)
 	intro_timer = intro_timer + dt
 
-	if intro_timer > 1 or love.keyboard.isDown("escape") then
+	if intro_timer > 0.65 or love.keyboard.isDown("escape") then
 		scene.switch("mainmenu")
 	end
 end
 
 function scenes.intro.draw()
-	love.graphics.draw(images.intro_card, 0, 0, 0, 1, 1)
+	love.graphics.draw(images.intro_raccoon, 0, 0, 0, 1, 1)
+	love.graphics.draw(images.intro_text, 0, 0, 0, 1, 1)
 end
