1af8b246df40ae11a4738d2906b191db82643b81
ROllerozxa <rollerozxa@voxelmanip.se>
2025-01-25 21:31:25 +0100

    Allow detecting newly created levels in selectlevel

--- level.lua
+++ level.lua
@@ -1,16 +1,13 @@
 -- Level handling functionality
 -- (TODO: Refactor more stuff into here)
 
-local totalLevels = nil
 function getTotalLevels()
-	if totalLevels == nil then
-		totalLevels = 0
-		while true do
-			if love.filesystem.getInfo("levels/"..(totalLevels+1)..".lua") then
-				totalLevels = totalLevels + 1
-			else
-				break
-			end
+	local totalLevels = 0
+	while true do
+		if love.filesystem.getInfo("levels/"..(totalLevels+1)..".lua") then
+			totalLevels = totalLevels + 1
+		else
+			break
 		end
 	end
 
