4dd513147f1ec04cadb1df387cf3d8bcca03d933
ROllerozxa <rollerozxa@voxelmanip.se>
2025-01-28 18:51:45 +0100

    Add playtime stat, and other statistics dialog fixes

--- scenes/statistics.lua
+++ scenes/statistics.lua
@@ -1,14 +1,14 @@
 -- Statistics dialog
 
-local statistics = {
+local stat = {
 	background = { 11, 75, 122 },
 	gui = Gui:new()
 }
 
-function statistics.init()
-	statistics.gui:add("back", Button:new{
-		x = 40, y = 40*14,
-		w = 40*12, h = 96,
+function stat.init()
+	stat.gui:add("back", Button:new{
+		x = 40, y = 40*15.4,
+		w = 40*12, h = 80,
 		label = S("Back to main menu"),
 		keybind = "escape",
 		onClick = function()
@@ -17,7 +17,31 @@ function statistics.init()
 	})
 end
 
-function statistics.back()
+local function formatPlaytime(seconds)
+	local units = {
+		{ "d", 86400 },
+		{ "h", 3600 },
+		{ "m", 60 },
+		{ "s", 1 }
+	}
+
+	local results = {}
+
+	for _, unit in ipairs(units) do
+		if seconds >= unit[2] then
+			local value = math.floor(seconds / unit[2])
+			seconds = seconds % unit[2]
+			table.insert(results, value .. unit[1])
+			if #results == 2 then
+				break
+			end
+		end
+	end
+
+	return table.concat(results, ", ")
+end
+
+function stat.back()
 	scene.switch("mainmenu")
 end
 
@@ -26,7 +50,7 @@ local coolColours = {coolRandomColour(), coolRandomColour()}
 
 local rows = {
 	{
-		label = "Boxes:",
+		label = S"Boxes:",
 		getValue = function()
 			return statistics.get("boxes")
 		end,
@@ -34,7 +58,7 @@ local rows = {
 			draw.box(x-65, y+17, 40, 40, angle, coolColours[1])
 		end
 	}, {
-		label = "Balls thrown:",
+		label = S"Balls thrown:",
 		getValue = function()
 			return statistics.get("balls")
 		end,
@@ -42,7 +66,7 @@ local rows = {
 			draw.ball(x-65, y+17, angle, coolColours[2])
 		end
 	}, {
-		label = "Levels unlocked:",
+		label = S"Levels unlocked:",
 		getValue = function()
 			return savegame.get("levelsUnlocked")
 		end,
@@ -50,16 +74,25 @@ local rows = {
 			love.graphics.draw(images.lock, x-65, y+17, angle, 0.5, 0.5, 64, 64)
 		end
 	}, {
-		label = "Levels played:",
+		label = S"Levels played:",
 		getValue = function()
 			return statistics.get("levels")
 		end,
-		onDraw = function()
+		onDraw = function(x, y)
+			love.graphics.draw(images.check, x-65, y+17, angle, 0.5, 0.5, 64, 64)
+		end
+	}, {
+		label = S"Total playtime:",
+		getValue = function()
+			return formatPlaytime(statistics.get("playtime"))
+		end,
+		onDraw = function(x, y)
+			love.graphics.draw(images.timer, x-65, y+17, angle, 0.5, 0.5, 64, 64)
 		end
 	}
 }
 
-function statistics.draw()
+function stat.draw()
 	love.graphics.setFont(fonts.sans.bigger)
 	printOutlined("Statistics", 40, 40, 3)
 
@@ -69,18 +102,18 @@ function statistics.draw()
 		local labelwidth = 450
 
 		love.graphics.setColor(love.math.colorFromBytes(9, 54, 87))
-		love.graphics.rectangle("fill", 40, 30+i*100, 1000, 75)
+		love.graphics.rectangle("fill", 40, 10+i*100, 1000, 75)
 
 		love.graphics.setColor(1,1,1)
-		love.graphics.print(row.label, offset, 50+i*100)
-		love.graphics.print(row.getValue(), offset+labelwidth, 50+i*100)
+		love.graphics.print(row.label, offset, 30+i*100)
+		love.graphics.print(row.getValue(), offset+labelwidth, 30+i*100)
 
 		if row.onDraw then
-			row.onDraw(offset, 50+i*100)
+			row.onDraw(offset, 30+i*100)
 		end
 	end
 
 	angle = angle + math.pi/128
 end
 
-return statistics
+return stat
