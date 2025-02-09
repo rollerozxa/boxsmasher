157b78a9bd7331fd87d93a0449ed6cf4806c6c9f
ROllerozxa <rollerozxa@voxelmanip.se>
2025-02-09 16:46:42 +0100

    Add 5 new levels

---
+++ levels/18.lua
@@ -0,0 +1,22 @@
+
+return {
+	author = 'ROllerozxa',
+	totalBalls = 2,
+	throwBoundary = {
+		x = 40*22, y = 40*6,
+		w = 40*5, h = 40*6,
+	},
+	boxclusters = {
+		{
+			x = 40*6, y = 40*4,
+			w = 40, h = 40,
+			aX = 5, aY = 12
+		},
+	},
+	terrain = {
+		{
+			x = 40*6, y = 40*16,
+			w = 40*5, h = 40*2
+		}
+	}
+}
---
+++ levels/19.lua
@@ -0,0 +1,25 @@
+
+return {
+	author = 'ROllerozxa',
+	totalBalls = 2,
+	throwBoundary = {
+		x = 40*3, y = 40*6,
+		w = 40*5, h = 40*6,
+	},
+	boxclusters = {
+		{
+			x = 40*12, y = 40*5,
+			w = 20, h = 20,
+			aX = 10, aY = 16
+		},
+	},
+	terrain = {
+		{
+			x = 40*12, y = 0,
+			w = 40*5, h = 40*4.95
+		}, {
+			x = 40*12, y = 40*13,
+			w = 40*5, h = 40*6
+		}
+	}
+}
